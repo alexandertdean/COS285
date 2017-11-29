31132c39d7ba26e67a3879b32438a52807d5b8c3
Alexander Dean (Desktop) <alexander.dean94@gmail.com>
2017-11-29 11:45:03 -0500

    randomletters

--- SongSearchEngine/student/SearchByLyricsPhrase.java
+++ SongSearchEngine/student/SearchByLyricsPhrase.java
@@ -13,21 +13,31 @@ public class SearchByLyricsPhrase {
 	
 	public static void main(String[] args) 
 	{
+		if (args.length == 0) {
+			System.out.println("Invalid number of arguments.");
+			return;
+		}
 		
+		SongCollection sc = new SongCollection(args[0]);
+		SearchByLyricsPhrase search = new SearchByLyricsPhrase(sc);
+		if (args.length > 1) {
+			System.out.println("Search for: " + args[1]);
+			Song[] result = search.search(args[1]);
+			System.out.println("Result size: " + result.length);
+		}
 	}
 	
 	public Song[] search(String phrase)
 	{
 		SearchByLyricsWords searchFn = new SearchByLyricsWords(sc);
 		Song[] unranked = searchFn.search(phrase);
-		int count = 0;
 		ArrayList<RankedItem> ranked = new ArrayList<RankedItem>();
 		for(Song s : unranked)
 		{
 			int rank = PhraseRanking.rankPhrase(s.getLyrics(), phrase);
 			if (rank > 0) 
 			{
-				ranked.add(new RankedItem(s, count));
+				ranked.add(new RankedItem(s, rank));
 			}
 			
 		}
@@ -36,11 +46,15 @@ public class SearchByLyricsPhrase {
 		CompareRank cmp = new CompareRank();
 		Arrays.sort(rankedArray, cmp);
 		Song[] result = new Song[rankedArray.length];
+		System.out.println("rank artist title");
 		for (int i = 0; i < rankedArray.length; i++)
 		{
+			if (i < Math.min(result.length,10)) {
+				System.out.println(rankedArray[i].count + " " + rankedArray[i].name.toString());
+			}
 			result[i] = (Song)(rankedArray[i].name);	
 		}
-		return new Song[0];
+		return result;
 	}
 	
 	public class CompareRank implements Comparator<RankedItem> {
